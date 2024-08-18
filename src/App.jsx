3f4d9f6519e158a8cd6ad2cc3aee76de91539339
ilushanchik <155296116+ilushanchik@users.noreply.github.com>
2024-08-19 00:35:32 +0300

    markup2

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 import userData from "./components/Profile/userData.json"
 import Profile from "./components/Profile/Profile"
-import friends from "./components/Profile/friends.json"
-import FriendList from "./components/Profile/FriendList"
-import transactions from "./components/Profile/transactions.json"
-import TransactionHistory from "./components/Profile/TransactionHistory"
+import friends from "./components/FriendList/friends.json"
+import FriendList from "./components/FriendList/FriendList"
+import transactions from "./components/TransactionHistory/transactions.json"
+import TransactionHistory from "./components/TransactionHistory/TransactionHistory"
 import Section from "./components/Section/Section";
 
 
